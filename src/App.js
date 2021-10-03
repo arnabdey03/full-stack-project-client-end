fef4449f74020b81bafe2b44f2eacb9d6d47355f
arnabdey03 <arnabdey021998@gmail.com>
2021-10-03 23:54:37 +0530

    add post via UI complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import {useDispatch} from "react-redux"
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getPosts } from "./actions/posts"
+import Posts from "./components/Posts/Posts"
+import Form from "./components/Form/Form"
+
+import memories from "./images/memories.png"
+import useStyles from "./styles"
+
+const App = () => {
+	const classes = useStyles()
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(getPosts())
+	}, [])
+
+	return (
+		<Container maxwidth="lg">
+
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography className={classes.heading} variant="h2" align="center">
+					Memories
+					<img className={classes.image} src={memories} alt="memories" height="60" />
+				</Typography>
+			</AppBar>
+
+			<Grow in>
+				<Container>
+					<Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+						<Grid item xs={12} sm={7}>
+							<Posts />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+
+		</Container>
+	)
 }
 
 export default App;
